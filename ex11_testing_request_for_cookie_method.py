a32bb83eba643a6732411b3d3fee61263ca6b263
Anastasia Skripnik <skripnikanastasiaia@gmail.com>
2022-03-22 23:14:48 +0300

    Ex11 Fixed

--- ex11_testing_request_for_cookie_method.py
+++ ex11_testing_request_for_cookie_method.py
@@ -6,11 +6,11 @@ class TestRequestCookieMethod:
         url = "https://playground.learnqa.ru/api/homework_cookie"
         response = requests.get(url)
         assert requests.cookies, "No cookie in response."
-        cookie_in_request = response.cookies
-        print(cookie_in_request)
-        str_cookie_in_request = str(cookie_in_request)
-        expected_cookie = "<RequestsCookieJar[<Cookie HomeWork=hw_value for .playground.learnqa.ru/>]>"
-        assert expected_cookie == str_cookie_in_request, "Cookies in response are differ the expected ones."
+        print(response.cookies)
+        assert response.cookies.get("HomeWork"), "No 'HomeWork' cookie in response."
+        cookie_in_request = response.cookies.get("HomeWork")
+        expected_cookie = {"HomeWork": "hw_value"}
+        assert expected_cookie["HomeWork"] == cookie_in_request, "Cookies from the response differ the expected ones."
 
 #python -m pytest -s ex11_testing_request_for_cookie_method.py
 
